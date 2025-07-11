538a40e51fbf1462396c3638c1e70f3215dd400f
hugo <vvhooping@gmail.com>
2025-07-11 11:29:23 +0900

    [Bronze V] Title: 윤년, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,7 +1,7 @@
 import sys
 year = int(sys.stdin.read())
 
-if (year%4==0 and year%100!=0) or (year%4==0 and year%400==0):
+if year%4==0 and (year%100!=0 or year%400==0):
     print(1)
 else:
     print(0)
