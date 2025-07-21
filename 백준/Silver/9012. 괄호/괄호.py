14681072efe7541aa2f1c189495bd5041570e864
hugo <vvhooping@gmail.com>
2025-07-21 22:04:59 +0900

    [Silver IV] Title: 괄호, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -4,30 +4,18 @@ test_case_count = int(input().strip())
 
 for _ in range(test_case_count):
     ps = sys.stdin.readline().strip()
+    count = 0
 
-    ps_list = []
+    for char in ps:
+        if char == '(':
+            count += 1
+        else:
+            count -= 1
 
-    for idx in range(len(ps)):
-        bracket = ps[idx]
+        if count < 0:
+            break
 
-        # 첫 번째 항목은 바로 담기
-        if len(ps_list) == 0:
-            ps_list.append(bracket)
-            continue
-
-        # 두 번째 항목부터 짝이 맞는지 비교하기
-        # 비교하는 괄호보다 먼저 들어간 괄호
-        last_bracket = ps_list[-1]
-
-        if last_bracket + bracket == "()": # 짝이 맞으면
-            ps_list.pop()
-
-        else: # 짝이 안 맞으면
-            ps_list.append(bracket)
-
-    # 모든 괄호에 대해 실행했는데, 리스트가 비어 있지 않다면
-    # 짝이 맞는 괄호를 찾는 데에 실패한 케이스가 있다는 것이다.
-    if len(ps_list):
-        print('NO')
-    else:
+    if count == 0:
         print('YES')
+    else:
+        print('NO')
