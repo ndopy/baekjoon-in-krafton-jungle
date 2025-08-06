5743c82a457da88c11a502f730b8e7e49484aca3
hugo <vvhooping@gmail.com>
2025-08-06 21:08:36 +0900

    [Bronze II] Title: 분해합, Time: 220 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,25 +1,14 @@
 import sys
 
 N = int(sys.stdin.readline())
-len_N = int(len(str(N)))
 
-result = []
+result = 0
 
-for num in range(1, N):
-    bbh_1 = num
-    bbh_2 = 0
+for i in range(1, N + 1):
+    sum_digits = sum(map(int, str(i)))
 
-    for i in range(len_N - 1, -1, -1):
-        bbh_2 += num // 10 ** i
-        num = num % 10 ** i
-
-    bbh = bbh_1 + bbh_2
-
-    if bbh == N:
-        result.append(bbh_1)
+    if i + sum_digits == N:
+        result = i
         break
 
-if not result:
-    print(0)
-else:
-    print(result[0])
+print(result)
