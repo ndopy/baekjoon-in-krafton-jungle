942ecfc35cff723dd362c1bdd533f14efa3b2b1a
hugo <vvhooping@gmail.com>
2025-09-15 14:30:54 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -3,14 +3,14 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 cards = list(map(int, sys.stdin.readline().split()))
 
-answers = []
+best_sum = 0
 
 for i in range(N - 2):
     for j in range(i + 1, N - 1):
         for k in range(j + 1, N):
-            sum_of_three_cards = cards[i] + cards[j] + cards[k]
+            current_sum = cards[i] + cards[j] + cards[k]
 
-            if sum_of_three_cards <= M:
-                answers.append(sum_of_three_cards)
+            if current_sum <= M and current_sum > best_sum:
+                best_sum = current_sum
 
-print(max(answers))
+print(best_sum)
