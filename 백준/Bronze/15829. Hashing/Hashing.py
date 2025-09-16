07d9be8c6841df03618b9cc202365c8c79bfcd75
hugo <vvhooping@gmail.com>
2025-09-16 09:58:54 +0900

    [Bronze II] Title: Hashing, Time: 88 ms, Memory: 108384 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,16 @@
+import sys
+
+L = int(sys.stdin.readline())
+chars = sys.stdin.readline()
+
+def alphabet_to_num(char):
+    # 'a'는 1이어야하므로 +1 처리를 해준다.
+    return ord(char) - ord('a') + 1
+
+H = 0
+
+for i in range(L):
+    num = alphabet_to_num(chars[i])
+    H += num * pow(31, i)
+
+print(H)
