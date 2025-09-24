3f6c13779ede1429cd00d5e4f044a7d593cce34d
hugo <vvhooping@gmail.com>
2025-09-24 19:52:14 +0900

    [Bronze III] Title: 지영 공주님의 마법 거울, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/11586. 지영 공주님의 마법 거울/지영 공주님의 마법 거울.py
+++ 백준/Bronze/11586. 지영 공주님의 마법 거울/지영 공주님의 마법 거울.py
@@ -13,14 +13,8 @@ emotion = int(sys.stdin.readline())
 if emotion == 1:
     print(*original, sep='\n')
 elif emotion == 2:
-    left_right_reversed = []
-
-    for line in original:
-        left_right_reversed.append("".join(reversed(line)))
-
+    left_right_reversed = [line[::-1] for line in original]
     print(*left_right_reversed, sep='\n')
-
 else:
-    up_down_reversed = list(reversed(original))
-
+    up_down_reversed = original[::-1]
     print(*up_down_reversed, sep='\n')
